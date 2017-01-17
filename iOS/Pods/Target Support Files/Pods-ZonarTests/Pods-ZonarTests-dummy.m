aac79b2a950134ad11be1eebf977f9d8aac097cf
Franz Ayestaran <franz@ayestaran.co.uk>
2017-01-17 12:57:18 +0000

    Started implementing messaging core
    17th January 2017

--- iOS/Pods/Target Support Files/Pods-ZonarTests/Pods-ZonarTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZonarTests : NSObject
-@end
-@implementation PodsDummy_Pods_ZonarTests
-@end
--- iOS/Pods/Target Support Files/Pods-ZonarUITests/Pods-ZonarUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZonarUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ZonarUITests
-@end
